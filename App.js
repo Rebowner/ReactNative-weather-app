ae2116ab615f1097e8c6f5e128a8dbe738389994
Rebowner <oliviahwaliaula@gmail.com>
2023-11-01 14:59:53 +0300

    weather app

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'; 
+import AppNavigation from './navigation/appNavigation.js';
+
+    
+  export default function App() {
+    return (
+       <AppNavigation />
+    );
+  }
+
+
